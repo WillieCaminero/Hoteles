9afb341f05d9487fed721aa8bb1377099c584041
WillieMn08 <WillieMn08@hotmail.com>
2016-07-03 12:45:58 -0400

    Eliminando carpeta Views

--- views/js/functions.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $( ".datepicker" ).datepicker();
-    $(".datepicker").datepicker({ minDate: 1 });
-	$(".datepicker").datepicker("option", "dateFormat", "dd/mm/yy");
-});
